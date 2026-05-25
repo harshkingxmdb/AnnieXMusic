bdb163e247d8baa034d613fa555df97acc82aa6e
𝐒𝐇𝐎𝐍𝐀 𝐁𝐎𝐓𝐒🚩 <harshkumar12038@gmail.com>
2026-05-25 23:42:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ DEEP_API = getenv("DEEP_API")      # optional
 
 # Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+YT_API_KEY = getenv("YT_API_KEY" , 'xbit_-G9GXbjRf8mA9_mz6-jnQcUoFpcEMsrM') ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
 # ── Hosting / deployment ───────────────────────────────────────────────────────
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
