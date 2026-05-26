f4fe8613ec3853e42098a4dae60a02a5c42c37d6
𝐒𝐇𝐎𝐍𝐀 𝐁𝐎𝐓𝐒🚩 <harshkumar12038@gmail.com>
2026-05-26 11:17:56 +0530

    Update Apple.py

--- AnnieXMedia/platforms/Apple.py
+++ AnnieXMedia/platforms/Apple.py
@@ -1,78 +1,71 @@
-﻿# Authored By Certified Coders © 2025
 import re
-from typing import List, Union, Optional
+from typing import Union
 
 import aiohttp
 from bs4 import BeautifulSoup
-from youtubesearchpython.aio import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 
 
 class AppleAPI:
     def __init__(self):
-        self.regex = r"^https:\/\/music\.apple\.com\/.+"
+        self.regex = r"^(https:\/\/music.apple.com\/)(.*)$"
         self.base = "https://music.apple.com/in/playlist/"
 
-    async def valid(self, link: str) -> bool:
-        return bool(re.search(self.regex, link or ""))
+    async def valid(self, link: str):
+        if re.search(self.regex, link):
+            return True
+        else:
+            return False
 
-    async def track(self, url: str, playid: Union[bool, str] = None):
+    async def track(self, url, playid: Union[bool, str] = None):
         if playid:
             url = self.base + url
-
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 if response.status != 200:
                     return False
                 html = await response.text()
-
         soup = BeautifulSoup(html, "html.parser")
-        title_query: Optional[str] = None
+        search = None
         for tag in soup.find_all("meta"):
-            if tag.get("property") == "og:title":
-                title_query = tag.get("content")
-                break
-
-        if not title_query:
+            if tag.get("property", None) == "og:title":
+                search = tag.get("content", None)
+        if search is None:
             return False
-
-        results = VideosSearch(title_query, limit=1)
-        data = await results.next()
-        if not data.get("result"):
-            return False
-
-        r = data["result"][0]
+        results = VideosSearch(search, limit=1)
+        for result in (await results.next())["result"]:
+            title = result["title"]
+            ytlink = result["link"]
+            vidid = result["id"]
+            duration_min = result["duration"]
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
         track_details = {
-            "title": r.get("title", ""),
-            "link": r.get("link", ""),
-            "vidid": r.get("id", ""),
-            "duration_min": r.get("duration"),
-            "thumb": r.get("thumbnails", [{}])[0].get("url", "").split("?")[0],
+            "title": title,
+            "link": ytlink,
+            "vidid": vidid,
+            "duration_min": duration_min,
+            "thumb": thumbnail,
         }
-        return track_details, track_details["vidid"]
+        return track_details, vidid
 
-    async def playlist(self, url: str, playid: Union[bool, str] = None):
+    async def playlist(self, url, playid: Union[bool, str] = None):
         if playid:
             url = self.base + url
-
-        try:
-            playlist_id = url.split("playlist/")[1]
-        except Exception:
-            return False
-
+        playlist_id = url.split("playlist/")[1]
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 if response.status != 200:
                     return False
                 html = await response.text()
-
         soup = BeautifulSoup(html, "html.parser")
         applelinks = soup.find_all("meta", attrs={"property": "music:song"})
-        results: List[str] = []
+        results = []
         for item in applelinks:
             try:
-                slug = item["content"].split("album/")[1].split("/")[0]
-                results.append(slug.replace("-", " "))
-            except Exception:
-                continue
-
+                xx = (((item["content"]).split("album/")[1]).split("/")[0]).replace(
+                    "-", " "
+                )
+            except:
+                xx = ((item["content"]).split("album/")[1]).split("/")[0]
+            results.append(xx)
         return results, playlist_id
