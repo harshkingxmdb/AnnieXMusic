39e460d8bc2ae3fcf65cecacd8f81305df721806
𝐒𝐇𝐎𝐍𝐀 𝐁𝐎𝐓𝐒🚩 <harshkumar12038@gmail.com>
2026-05-26 12:54:36 +0530

    Update Youtube.py

--- AnnieXMedia/platforms/Youtube.py
+++ AnnieXMedia/platforms/Youtube.py
@@ -15,9 +15,9 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from youtubesearchpython.__future__ import VideosSearch, CustomSearch
 import base64
-from VIVAANXMUSIC import LOGGER
-from VIVAANXMUSIC.utils.database import is_on_off
-from VIVAANXMUSIC.utils.formatters import time_to_seconds
+from AnnieXMedia import LOGGER
+from AnnieXMedia.utils.database import is_on_off
+from AnnieXMedia.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
 logger = LOGGER(__name__)
