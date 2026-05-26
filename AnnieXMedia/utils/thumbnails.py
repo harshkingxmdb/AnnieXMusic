26e8250b17c24cf91626e96723ccfa3b1471094f
𝐒𝐇𝐎𝐍𝐀 𝐁𝐎𝐓𝐒🚩 <harshkumar12038@gmail.com>
2026-05-26 13:00:32 +0530

    Update thumbnails.py

--- AnnieXMedia/utils/thumbnails.py
+++ AnnieXMedia/utils/thumbnails.py
@@ -1,10 +1,9 @@
-﻿# Authored By Certified Coders © 2025
 import os
 import re
 import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from youtubesearchpython.aio import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 from config import YOUTUBE_IMG_URL
 from AnnieXMedia.core.dir import CACHE_DIR 
 
@@ -92,8 +91,8 @@ async def get_thumb(videoid: str) -> str:
     # Draw details
     draw = ImageDraw.Draw(bg)
     try:
-        title_font = ImageFont.truetype("AnnieXMedia/assets/thumb/font2.ttf", 32)
-        regular_font = ImageFont.truetype("AnnieXMedia/assets/thumb/font.ttf", 18)
+        title_font = ImageFont.truetype("VIVAANXMUSIC/assets/thumb/font2.ttf", 32)
+        regular_font = ImageFont.truetype("VIVAANXMUSIC/assets/thumb/font.ttf", 18)
     except OSError:
         title_font = regular_font = ImageFont.load_default()
 
@@ -115,7 +114,7 @@ async def get_thumb(videoid: str) -> str:
     draw.text((BAR_X + BAR_TOTAL_LEN - (90 if is_live else 60), BAR_Y + 15), end_text, fill="red" if is_live else "black", font=regular_font)
 
     # Icons
-    icons_path = "AnnieXMedia/assets/thumb/play_icons.png"
+    icons_path = "VIVAANXMUSIC/assets/thumb/play_icons.png"
     if os.path.isfile(icons_path):
         ic = Image.open(icons_path).resize((ICONS_W, ICONS_H)).convert("RGBA")
         r, g, b, a = ic.split()
